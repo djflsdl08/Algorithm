59e8ba92c76429c4f27bdb69de8a3b50dd6f81eb
djflsdl08 <djflsdl08@naver.com>
2016-09-07 23:43:15 +0900

    Add Circular Linked List.

--- main.cpp
+++ main.cpp
@@ -14,15 +14,19 @@ void error(char *message){
 
 ListNode* create_node(element data,ListNode *link){
     ListNode *new_node;
-    new_node = (ListNode *) malloc(sizeof(ListNode));
-    if(new_node==NULL) error("memory allocation error");
-    new_node->data = data;
-    new_node->link = link;
+    new_node = (ListNode *)malloc(sizeof(ListNode));
+    if(new_node==NULL) {
+        error("memory allocation error");
+        exit(1);
+    }
+    new_node -> link = link;
+    new_node -> data = data;
     return (new_node);
 }
 
 void display(ListNode *head){
-    ListNode *p = head;
+    ListNode *p;
+    p = head;
     while(p!=NULL){
         printf("%d -> ",p->data);
         p = p->link;
@@ -31,8 +35,8 @@ void display(ListNode *head){
 }
 
 void insert_node(ListNode **phead,ListNode *p,ListNode *new_node){
-    if(*phead==NULL){
-        new_node ->link = NULL;
+    if(*phead == NULL){
+        new_node -> link = NULL;
         *phead = new_node;
     }
     else if(p==NULL){
@@ -40,17 +44,17 @@ void insert_node(ListNode **phead,ListNode *p,ListNode *new_node){
         *phead = new_node;
     }
     else {
-        new_node->link = p -> link;
-        p->link = new_node;
+        new_node -> link = p -> link;
+        p -> link = new_node;
     }
 }
 
 void remove_node(ListNode **phead,ListNode *p,ListNode *removed){
     if(p==NULL){
-        *phead = (*phead)->link;
+        *phead = (*phead) -> link;
     }
-    else{
-        p -> link = removed -> link;
+    else {
+        p->link = removed -> link;
     }
     free(removed);
 }
@@ -77,22 +81,81 @@ ListNode* reverse(ListNode *head){
         r = q;
         q = p;
         p = p->link;
-        q -> link = r;
+        q ->link = r;
     }
     return q;
 }
+
 ListNode* search(ListNode *head,element data){
-    ListNode *p;
-    p = head;
+    ListNode *p = head;
     while(p!=NULL){
-        if(p->data==data) return p;
+        if(p->data == data) return p;
         p = p->link;
     }
     return p;
 }
 
+void insert_first(ListNode **phead,ListNode *node){
+    if(*phead == NULL){
+        *phead = node;
+        node -> link = node;
+    }
+    else {
+        node -> link = (*phead) -> link;
+        (*phead) -> link = node;
+    }
+}
+
+void insert_last(ListNode **phead,ListNode *node){
+    if(*phead == NULL){
+        *phead = node;
+        node -> link = node;
+    }
+    else {
+        node -> link = (*phead) -> link;
+        (*phead) -> link = node;
+        (*phead) = node;
+    }
+}
+
+void display_circular(ListNode *head){
+    ListNode *p;
+    if(head == NULL) return;
+    p = head;
+    do {
+        printf("%d -> ",p->data);
+        p = p->link;
+    }while(p!=head);
+    printf("\n");
+}
+
+void remove_first(ListNode **head){
+    ListNode *p = (*head)->link;
+    if(p==(*head)){
+        *head =NULL;
+    }
+    else {
+        (*head) -> link = p -> link;
+    }
+    free(p);
+}
+
+void remove_last(ListNode **head){
+    ListNode *p = *head;
+    if(p == p -> link){
+        *head = NULL;
+    }
+    else {
+        while(p->link!=*head){
+            p = p -> link;
+        }
+        p -> link = (*head) -> link;
+        *head = p;
+    }
+    free(p);
+}
 int main() {
-    ListNode *list1=NULL,*list2=NULL;
+    ListNode *list1=NULL,*list2=NULL,*list3=NULL;
     ListNode *p;
 
     //List1 = 30->20->10
@@ -120,4 +183,17 @@ int main() {
 
     p = search(list1,20);
     printf("탐색성공:%d\n",p->data);
-}
+
+    //list3 : Circular Linked list
+    insert_first(&list3,create_node(10,NULL));
+    insert_first(&list3,create_node(20,NULL));
+    insert_first(&list3,create_node(40,NULL));
+    insert_last(&list3,create_node(30,NULL));
+    insert_last(&list3,create_node(50,NULL));
+    display_circular(list3);
+
+    remove_first(&list3);
+    display_circular(list3);
+    remove_last(&list3);
+    display_circular(list3);
+ }
